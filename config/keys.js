1b2ccf128b562b5f31f804b22afdd48e47448f13
Vibhor Singh <bibhorsingh2002@gmail.com>
2022-04-09 16:25:42 +0530

    added files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: '1065811822514-05k8d54ja3o02nvqtohvfsgp3v7or5lm.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://user1:bJfeYsTI3h1MrMrj@reviewit.o4byx.mongodb.net/ReviewItDB?retryWrites=true&w=majority',
+};
